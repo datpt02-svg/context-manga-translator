ed309ce1019ea1222a86fd4dc3ff4c8efbd83459
datpt02 <datpt02@ominext.com>
2026-07-14 17:33:26 +0700

    feat: add onWheel handler to Input component for number type

--- ui/components/ui/input.tsx
+++ ui/components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
+function Input({ className, type, onWheel, ...props }: React.ComponentProps<'input'>) {
   return (
     <input
       type={type}
@@ -13,6 +13,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
         'aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40',
         className,
       )}
+      onWheel={type === 'number' ? (e) => { e.preventDefault(); e.currentTarget.blur() } : onWheel}
       {...props}
     />
   )
